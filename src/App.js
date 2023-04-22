2b847dcd0589ae366cb25ed0a855936488800466
Gowtham <gowthamsgobi@gmail.com>
2023-04-22 16:00:59 +0530

    basic layout designed with css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+// --> app component
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="searchContainer">
+          <p>searchBox section</p>
+        </div>
+        <div className="bodyOfTheContent">
+          <div className="leftSection">01</div>
+          <div className="rightSection">02</div>
+        </div>
+        <div className="disclaimerContainer">
+          <p>disclaimer section</p>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
