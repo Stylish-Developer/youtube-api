32523be0fbb6c1afc09bd299d910aa505e3a55b7
Gautham <95676034+Stylish-Developer@users.noreply.github.com>
2023-04-22 20:45:03 +0530

    search box ui designed with basic functionality and folder structure used (#2)

---
+++ src/components/searchbox/index.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Input } from "../ui/input";
+
+import SearchSvg from "../../assets/svg/search-outline.svg";
+
+import "../../assets/css/searchB.css";
+
+// --> search component
+const SearchBox = () => {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="SearchBContainer">
+        <div className="innerDiv">
+          <img
+            src={SearchSvg}
+            height={20}
+            width={20}
+            alt="searchIcon"
+            className="searchIcon"
+          />
+          <Input
+            name={inputText}
+            value={inputText}
+            onChange={handleChange}
+            placeholder="Search"
+          />
+        </div>
+        {JSON.stringify(inputText)}
+      </div>
+    </>
+  );
+};
+
+export default SearchBox;
---
+++ src/components/ui/button.js
@@ -0,0 +1,11 @@
+//button component
+const Button = (props) => {
+    const { label } = props;
+    return(
+        <>
+        <button>{label}</button>
+        </>
+    )
+}
+
+export default Button;
